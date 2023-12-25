de7d75089a814b0a54588d1b518f0a1b24719bc0
Eugene-Fed <eugene.a.fed@gmail.com>
2023-12-25 20:19:00 +0300

    Финальный коммит по решенной задаче

--- README.md
+++ README.md
@@ -3,29 +3,26 @@
 ## Start and Config
 - В корне необходимо скопировать файл `example.env` и вставить в ту же папку с именем `.env`.
 - Указать корректные токены в соответствующие переменные.  
-`DEVMAN_TOKEN` - токен пользователя API в Девман  
-`TG_BOT_TOKEN`- токен ТГ бота  
-`TG_CHAT_ID` - ID чата в личных сообщениях, в который бот будет отправлять оповещения  
+`MAIL_SENDER_LOGIN` - Логин почты с которой будут отправлены сообщения  
+`TMAIL_SENDER_PASSWORD`- Пароль почты отправителя  
 
 ## Features
-- Получить имя пользователя:
-```
-updates = bot.get_updates()
-firts_name = updates[-1].message.from_user.first_name
-```
-- Используй файл `config/config.json` для настройки версии апи, задержек и текста отправляемых сообщений.
+- Используй файл `config/config.json` для настройки реферральной ссылки, заголовков сообщения, списка клиентских данных и параметров SMPT-сервера.
 
 ## Config example
 ```
 {
   "urls":
   {
-    "user_reviews": "https://dvmn.org/api/long_polling/",
+    "ref_link": "https://dvmn.org/referrals/cHqg6YGb5iTvYMcW1rZJLctziAfOhbwepKOOHwwM/"
   },
-  "timeout": 120,
-  "sleep": 30,
-  "message": "Mentor has checked your work!\nTheme: {title}\nLink: {url}\n\n{review}",
-  "review_messages": ["Unfortunately there are errors in the work :(",
-    "Everything is fine! You can proceed to the next lesson :)"],
+  "mail_headers": {"sender_name": "Евгений", "subject": "Приглашение"},
+  "clients": [
+      {"name": "Александр", "mail": "test@mail.ru"}
+  ],
+  "smtp":
+  [
+      {"url": "smtp.yandex.ru", "port": 465}
+  ]
 }
 ```
--- main.py
+++ main.py
@@ -1,23 +1,39 @@
 import smtplib
-import time
-import requests
 import os
 import json
-import telegram
-import logging
 
 from dotenv import load_dotenv
 
 
-def main():
-    server = smtplib.SMTP_SSL('smtp.yandex.ru', 465)
+def load_config():
+    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+    config_path = os.path.join(os.path.dirname(__file__), 'config', 'config.json')
 
-    ref_link = r'https://dvmn.org/referrals/cHqg6YGb5iTvYMcW1rZJLctziAfOhbwepKOOHwwM/'
-    friend_name = r'Александр'
-    my_name = r'Юджин'
-    from_address = r'me@e-fed.ru'
-    to_address = r'petr@yandex.ru'
-    mail_subject = r'Приглашение!'
+    if os.path.exists(dotenv_path):
+        load_dotenv(dotenv_path)
+    if not os.environ.get('MAIL_SENDER_LOGIN') or not os.environ.get('MAIL_SENDER_PASSWORD'):
+        raise EnvironmentError("You should set your `.env` file. Please check README.md.")
+    if os.path.exists(config_path):
+        return json.load(open(config_path, "r", encoding="utf-8"))
+    else:
+        raise FileNotFoundError("File `config/config.json` not found. Please check README.md.")
+
+
+def main():
+    config = load_config()
+    sender_login = os.environ['MAIL_SENDER_LOGIN']
+    sender_password = os.environ['MAIL_SENDER_PASSWORD']
+
+    server = smtplib.SMTP_SSL(config['smtp'][0]['url'], config['smtp'][0]['port'])
+    server.login(sender_login, sender_password)
+
+    ref_link = config['urls']['ref_link']
+    client_name = config['clients'][0]['name']
+    client_address = config['clients'][0]['mail']
+
+    sender_name = config['mail_headers']['sender_name']
+    sender_address = os.environ['MAIL_SENDER_LOGIN']
+    mail_subject = config['mail_headers']['subject']
 
     with open('mail_text.txt', 'r', encoding="utf-8") as f:
         mail_text = f.read()
@@ -25,16 +41,18 @@ def main():
         mail_template = f.read()
 
     mail_text = mail_text.replace(r'%website%', ref_link)
-    mail_text = mail_text.replace(r'%friend_name%', friend_name)
-    mail_text = mail_text.replace(r'%my_name%', my_name)
+    mail_text = mail_text.replace(r'%friend_name%', client_name)
+    mail_text = mail_text.replace(r'%my_name%', sender_name)
 
     letter = mail_template.format(
-        from_address=from_address,
-        to_address=to_address,
+        from_address=sender_address,
+        to_address=client_address,
         subject=mail_subject,
         text=mail_text)
     letter = letter.encode("UTF-8")
-    print(letter)
+
+    server.sendmail(sender_address, client_address, letter)
+    server.quit()
 
 
 if __name__ == '__main__':
